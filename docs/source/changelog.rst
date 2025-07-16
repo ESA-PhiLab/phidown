d53a1371947000ad3b97d288b9ef8d494d08a048
roberto_esaclear <roberto.delprete@esa.int>
2025-07-16 15:35:34 +0200

    Bump version to 0.1.16 in changelog, configuration, and project files

--- docs/source/changelog.rst
+++ docs/source/changelog.rst
@@ -24,7 +24,7 @@ and this project adheres to `Semantic Versioning <https://semver.org/spec/v2.0.0
 - Documentation build process
 - API reference generation
 
-[0.1.15] - 2024-07-15
+[0.1.16] - 2024-07-15
 ---------------------
 
 ### Added
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,8 +15,8 @@ sys.path.insert(0, os.path.abspath('../../'))
 project = 'Φ-Down'
 copyright = '2025, Roberto Del Prete'
 author = 'Roberto Del Prete'
-release = '0.1.15'
-version = '0.1.15'
+release = '0.1.16'
+version = '0.1.16'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
